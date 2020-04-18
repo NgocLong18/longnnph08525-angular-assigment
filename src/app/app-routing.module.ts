d3085e9ab57058cf0c2c2dd80ed878e79cbc2254
NgocLong18 <akaishuu1357@gmail.com>
2020-04-18 12:05:59 +0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { HomeComponent } from './home/home.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ProductAddComponent } from "./product-add/product-add.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { HomeComponent } from "./home/home.component";
+import { ProductEditComponent } from "./product-edit/product-edit.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 const routes: Routes = [
-  {path : '', redirectTo: 'home', pathMatch: 'full'},
-  {path : 'home', component:HomeComponent},
-  {path : 'product-manager', component:ProductListComponent},
-  {path: 'product/:id', component: ProductDetailComponent},
-  { path: 'product-add', component: ProductAddComponent },
-  { path: 'product-manager/product-edit/:id', component: ProductEditComponent },
-  { path: '**', component: NotfoundComponent }
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "admin", component: SidebarComponent,
+    children: [
+      { path: "", redirectTo: "dashboard",pathMatch: "full"},
+      { path: "dashboard", component: ProductListComponent},
+      { path: "dashboard/product-add", component: ProductAddComponent },
+      { path: "dashboard/product-edit/:id", component: ProductEditComponent}
+    ]
+  },
+  { path: "product/:id", component: ProductDetailComponent },
+
+  { path: "**", component: NotfoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
